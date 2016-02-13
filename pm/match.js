5aab6bb16d4d429b345904a5a53a80fe329dc8ea
Darin Minamoto <darinm223@gmail.com>
2016-02-12 20:04:15 -0800

    Added homework 3

--- pm/match.js
+++ pm/match.js
@@ -1,6 +1,188 @@
-'use strict';
+'use strict'
 
-function match(value /* pat1, fun1, pat2, fun2, ... */) {
-  throw new TODO('match not implemented');
+function Wildcard () {}
+
+function InstanceOf (C, ps) {
+  this.C = C
+  this.ps = ps
+}
+
+function Predicate (fn) {
+  this.fn = fn
+}
+
+function Many (ps) {
+  this.ps = ps
+}
+
+var _ = new Wildcard()
+
+function instof () {
+  if (arguments.length < 1) {
+    throw new EvalError('instof needs at least one argument')
+  }
+
+  var args = [].slice.call(arguments)
+
+  var C = args.shift()
+  var ps = args
+
+  return new InstanceOf(C, ps)
+}
+
+function pred (fn) {
+  return new Predicate(fn)
+}
+
+function many () {
+  if (arguments.length === 0) {
+    return new Many([])
+  } else {
+    var args = arguments[0]
+    if (args.constructor === Array) {
+      return new Many(args)
+    } else {
+      return new Many([args])
+    }
+  }
+}
+
+function matchMany (manyPattern, value) {
+  var patternIndex = 0
+  var args = []
+
+  // initalize the array of arrays
+  for (var i = 0; i < manyPattern.ps.length; i++) {
+    args.push([])
+  }
+
+  while (value.length > 0) {
+    var subvalue = value[0]
+    var subpattern = manyPattern.ps[patternIndex]
+    var subargs = null
+    if (subpattern instanceof Many) {
+      subargs = matchMany(subpattern, subvalue)
+      if (subargs === null) {
+        // return the rest of values
+        return args
+      } else {
+        // consume first element if pattern matches
+        value.shift()
+
+        for (var x = 0; x < subargs.length; x++) {
+          args[patternIndex].push(subargs[x])
+        }
+      }
+    } else {
+      subargs = matchPattern(subpattern, subvalue)
+      if (subargs === null) {
+        // return the rest of values
+        return args
+      } else {
+        // consume first element if pattern matches
+        value.shift()
+
+        for (var j = 0; j < subargs.length; j++) {
+          if (subargs[j].constructor === Array) {
+            for (var k = 0; k < subargs[j].length; k++) {
+              args[k].push(subargs[j][k])
+            }
+          } else {
+            args[j].push(subargs[j])
+          }
+        }
+      }
+    }
+
+    patternIndex += 1
+    if (patternIndex >= manyPattern.ps.length) {
+      patternIndex = 0
+    }
+  }
+
+  return args
+}
+
+function matchPattern (pattern, value) {
+  var args = null
+  if (pattern instanceof Wildcard) {
+    args = [value]
+  } else if (pattern instanceof Predicate) {
+    if (pattern.fn(value) === true) {
+      args = [value]
+    }
+  } else if (pattern instanceof InstanceOf) {
+    var instanceCheck = value instanceof pattern.C
+    args = matchPattern(pattern.ps, pattern.C.prototype.deconstruct.call(value))
+
+    if (instanceCheck !== true) {
+      args = null
+    }
+  } else if (pattern.constructor === Array && value.constructor === Array) {
+    var newArgs = []
+    var matched = true
+
+    for (var i = 0; i < pattern.length; i++) {
+      var subpattern = pattern[i]
+      var subvalue = value[i]
+      var subargs = []
+
+      if (subpattern instanceof Many) {
+        var subarray = value.slice(i, value.length)
+        subargs = matchMany(subpattern, subarray)
+        value = value.slice(0, i + 1).concat(subarray)
+
+        if (pattern.slice(i + 1, pattern.length).length >
+            value.slice(i + 1, value.length).length) {
+          throw new EvalError('There are more patterns than values')
+        }
+      }
+
+      subargs.push.apply(subargs, matchPattern(subpattern, subvalue))
+
+      if (subargs === null) {
+        matched = false
+        break
+      }
+
+      newArgs.push.apply(newArgs, subargs)
+    }
+
+    if (matched === true) {
+      args = newArgs
+    }
+  } else {
+    if (pattern === value) {
+      args = []
+    }
+  }
+
+  return args
+}
+
+function match () {
+  if (arguments.length <= 0) {
+    throw new EvalError('Argument\'s length is too small')
+  }
+
+  var value = arguments[0]
+
+  for (var i = 1; i < arguments.length; i += 2) {
+    var pattern = arguments[i]
+    var expression = arguments[i + 1]
+
+    // Handle patterns
+    var args = matchPattern(pattern, value)
+
+    if (args === null) {
+      continue
+    }
+
+    if (args.length === expression.length) {
+      return expression.apply(null, args)
+    } else {
+      throw new EvalError('Arity doesn\'t match for function')
+    }
+  }
 }
 
--- pm/tests.js
+++ pm/tests.js
@@ -36,89 +36,150 @@ class Point3D extends Point {
 // Tests!
 
 tests(JS,
-  {
-    name: 'wildcard',
-    code: 'match(123,\n' +
+      {
+        name: 'wildcard',
+        code: 'match(123,\n' +
           '  _,\n' +
-          '   x => x + 2\n' +
-          ')',
-    expected: 125
-  },
-  {
-    name: 'literal pattern',
-    code: 'match(123,\n' +
+            '   x => x + 2\n' +
+              ')',
+              expected: 125
+      },
+      {
+        name: 'literal pattern',
+        code: 'match(123,\n' +
           '  42,\n' +
-          '    () => "aaa",\n' +
-          '  123,\n' +
-          '    () => "bbb",\n' +
-          '  444,\n' +
-          '    () => "ccc"\n' +
-          ')',
-    expected: "bbb"
-  },
-  {
-    name: 'array pattern',
-    code: 'match(["+", 5, 7],\n' +
+            '    () => "aaa",\n' +
+              '  123,\n' +
+                '    () => "bbb",\n' +
+                  '  444,\n' +
+                    '    () => "ccc"\n' +
+                      ')',
+                      expected: "bbb"
+      },
+      {
+        name: 'array pattern',
+        code: 'match(["+", 5, 7],\n' +
           '  ["+", _, _],\n' +
-          '    (x, y) => x + y\n' +
-          ')',
-    expected: 12
-  },
-  {
-    name: 'class pattern',
-    code: 'match(new Point3D(1, 2, 3),\n' +
+            '    (x, y) => x + y\n' +
+              ')',
+              expected: 12
+      },
+      {
+        name: 'class pattern',
+        code: 'match(new Point3D(1, 2, 3),\n' +
           '  instof(Point3D, _, 2, _),\n' +
-          '    (x, z) => x + z\n' +
-          ')',
-    expected: 4
-  },
-  {
-    name: 'many',
-    code: 'match([[1, 2], [3, 4], [5, 6]],\n' +
+            '    (x, z) => x + z\n' +
+              ')',
+              expected: 4
+      },
+      {
+        name: 'many',
+        code: 'match([[1, 2], [3, 4], [5, 6]],\n' +
           '  [many([_, _])],\n' +
-          '    (xs,ys) => [xs, ys]\n' +
-          ')',
-    expected: [[1,3,5], [2,4,6]]
-  },
-  {
-    name: 'pred',
-    code: 'match(5,\n' +
+            '    (xs,ys) => [xs, ys]\n' +
+              ')',
+              expected: [[1,3,5], [2,4,6]]
+      },
+      {
+        name: 'pred',
+        code: 'match(5,\n' +
           '  pred(greaterThan(8)),\n' +
-          '    x => x + " is greater than 8",\n' +
-          '  pred(greaterThan(2)),\n' +
-          '    x => x + " is greater than 2"\n' +
-          ')',
-    expected: "5 is greater than 2"
-  },
-  {
-    name: 'many and pred',
-    code: 'match(["sum", 1, 2, 3, 4],\n' +
+            '    x => x + " is greater than 8",\n' +
+              '  pred(greaterThan(2)),\n' +
+                '    x => x + " is greater than 2"\n' +
+                  ')',
+                  expected: "5 is greater than 2"
+      },
+      {
+        name: 'many and pred',
+        code: 'match(["sum", 1, 2, 3, 4],\n' +
           '  ["sum", many(pred(isNumber))],\n' +
-          '    ns => ns.reduce((x, y) => x + y)\n' +
-          ')',
-    expected: 10
-  },
-  {
-    name: 'first match wins',
-    code: 'match(123,\n' +
+            '    ns => ns.reduce((x, y) => x + y)\n' +
+              ')',
+              expected: 10
+      },
+      {
+        name: 'first match wins',
+        code: 'match(123,\n' +
           '  _,\n' +
-          '   x => x,\n' +
-          '  123,\n' +
-          '    () => 4\n' +
-          ')',
-    expected: 123
-  },
-  {
-    name: 'match failed',
-    code: 'match(3,\n' +
+            '   x => x,\n' +
+              '  123,\n' +
+                '    () => 4\n' +
+                  ')',
+                  expected: 123
+      },
+      {
+        name: 'match failed',
+        code: 'match(3,\n' +
           '  1,\n' +
-          '    () => 1,\n' +
-          '  2,\n' +
-          '    () => 2,\n' +
-          '  [3],\n' +
-          '    () => 3\n' +
-          ')',
-    shouldThrow: true
-  }
-);
+            '    () => 1,\n' +
+              '  2,\n' +
+                '    () => 2,\n' +
+                  '  [3],\n' +
+                    '    () => 3\n' +
+                      ')',
+                      shouldThrow: true
+      },
+      {
+        name: 'remains pattern',
+        code: 'match([1, 2, 3, "and", 4],\n'+
+          '[many(_), "and", _],\n'+
+            '(xs, x) => xs\n'+
+              ')',
+              shouldThrow: true
+       },
+      {
+        name: 'many and wildcard',
+        code: 'match([1, 2, 3, "and", 4],\n'+
+          '[many(pred(isNumber)), "and", _],\n'+
+            '(xs, x) => xs.concat(x)\n'+
+              ')',
+              expected:[1,2,3,4]
+      },
+      {
+        name: 'null match',
+        code: 'match([],[many([_,_])],(x,y) => [x,y])',
+        expected:[[],[]]
+      },
+      {
+        name: 'many with pred',
+        code: 'match([1, 2, 3, "and", 4],\n'+
+          '[many(pred(isNumber)), "or"],\n'+
+            '(xs, x) => xs\n'+
+              ')',
+              shouldThrow: true
+      },
+      {
+        name: 'many pred and many wildcard',
+        code: 'match([1, 2, 3, "and", 4],\n'+
+          '[many(pred(isNumber)), many(_)],\n'+
+            '(xs, x) => xs\n'+
+              ')',
+              expected:[1,2,3]
+      },
+      {
+        name: 'arity check',
+        code: 'match(new Point3D(1, 2, 3),\n'+
+          'instof(Point3D, _, 2, _),\n'+
+            '(x,y,z) => x + y + z\n'+
+              ')',
+              shouldThrow: true
+      },
+      {
+        name: 'wildcard, many pred and many wildcard',
+        code: 'match([1, 2, 3, "and", 4],\n'+
+          '[_,2,many(pred(isNumber)), many(_)],\n'+
+            '(x, xs, y) => xs\n'+
+              ')',
+              expected:[3]
+      },
+      {
+        name: 'nested many',
+        code: 'match([[1,2], [4,5,6], [7]],\n' +
+          '  [many([many(pred(isNumber))])],\n' +
+            '   x => x\n' +
+              ')',
+              expected: [[1,2], [4,5,6], [7]]
+      }
+     );
 
