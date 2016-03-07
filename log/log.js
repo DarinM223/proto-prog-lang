ad6b2c8cbe4e579e3e0c3ace04ef7a3dceb158a5
Darin Minamoto <darinm223@gmail.com>
2016-03-06 23:19:44 -0800

    Implemented makeCopyWithFreshVarNames

--- log/log.js
+++ log/log.js
@@ -1,33 +1,47 @@
+/* global Rule, Clause, Var, Subst, Program, TODO */
+
 // -----------------------------------------------------------------------------
 // Part I: Rule.prototype.makeCopyWithFreshVarNames() and
 //         {Clause, Var}.prototype.rewrite(subst)
 // -----------------------------------------------------------------------------
 
-Rule.prototype.makeCopyWithFreshVarNames = function() {
-  throw new TODO('Rule.prototype.makeCopyWithFreshVarNames not implemented');
-};
+Rule.prototype.makeCopyWithFreshVarNames = function () {
+  return new Rule(this.head.makeCopyWithFreshVarNames(), this.body.map(function (arg) {
+    return arg.makeCopyWithFreshVarNames()
+  }))
+}
 
-Clause.prototype.rewrite = function(subst) {
-  throw new TODO('Clause.prototype.rewrite not implemented');
-};
+Clause.prototype.makeCopyWithFreshVarNames = function () {
+  return new Clause(this.name, this.args.map(function (arg) {
+    return arg.makeCopyWithFreshVarNames()
+  }))
+}
 
-Var.prototype.rewrite = function(subst) {
-  throw new TODO('Var.prototype.rewrite not implemented');
-};
+Var.prototype.makeCopyWithFreshVarNames = function () {
+  return new Var(this.name + '_')
+}
+
+Clause.prototype.rewrite = function (subst) {
+  throw new TODO('Clause.prototype.rewrite not implemented')
+}
+
+Var.prototype.rewrite = function (subst) {
+  throw new TODO('Var.prototype.rewrite not implemented')
+}
 
 // -----------------------------------------------------------------------------
 // Part II: Subst.prototype.unify(term1, term2)
 // -----------------------------------------------------------------------------
 
-Subst.prototype.unify = function(term1, term2) {
-  throw new TODO('Subst.prototype.unify not implemented');
-};
+Subst.prototype.unify = function (term1, term2) {
+  throw new TODO('Subst.prototype.unify not implemented')
+}
 
 // -----------------------------------------------------------------------------
 // Part III: Program.prototype.solve()
 // -----------------------------------------------------------------------------
 
-Program.prototype.solve = function() {
-  throw new TODO('Program.prototype.solve not implemented');
-};
+Program.prototype.solve = function () {
+  throw new TODO('Program.prototype.solve not implemented')
+}
 
